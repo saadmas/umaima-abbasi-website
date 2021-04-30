fe7c6eda5bbada20f512beaf50a118a9fdc70b0a
Saad <saadmasood96@hotmail.com>
2021-04-30 16:14:40 -0400

    add resume link to NavBar

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -8,6 +8,7 @@ const NavBar = () => {
         <span className={styles.navLink}>Work</span>
         <span className={styles.navLink}>Leadership</span>
         <span className={styles.navLink}>Interests</span>
+        <span className={styles.navLink}>Resume</span>
         <span className={styles.navLink}>About Me</span>
       </div>
     </div>
